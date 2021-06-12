dfa6caf9f49f89621382b312c5f51c9fb4f4a9a6
jonyszone <jonyszone@gmail.com>
2021-06-12 09:50:14 +0600

    increment added

--- app/src/main/java/com/example/taptoincrement/MainActivity.kt
+++ app/src/main/java/com/example/taptoincrement/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.taptoincrement
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            count += 1000
+            valueTV.text = "$$count"
+
+        }
     }
 }
