d3530ff8ccc852a5337428a1fa0c2ca5ab331119
jonyszone <jonyszone@gmail.com>
2021-11-16 10:31:11 +0600

    added viewBinding

--- app/src/main/java/com/example/taptoincrement/MainActivity.kt
+++ app/src/main/java/com/example/taptoincrement/MainActivity.kt
@@ -1,53 +1,64 @@
+
 package com.example.taptoincrement
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.view.View.inflate
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
+import com.example.taptoincrement.databinding.ActivityMainBinding
+import com.example.taptoincrement.databinding.ActivityMainBinding.bind
+import com.example.taptoincrement.databinding.ActivityMainBinding.inflate
+
 
 class MainActivity : AppCompatActivity() {
-    private val Tag: String? = "Showing Info"
+    private lateinit var binding: ActivityMainBinding
+    private val TAG: String = "Showing Info"
     var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = inflate(LayoutInflater.from(this))
+        val view = binding.root
+        setContentView(view)
 
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             count += 1000
-            valueTV.text = "$$count"
+            binding.valueTV.text = "$$count"
 
             when (count){
                 10000 -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    valueTV.setTextColor(getColor(R.color.teal_700))
+                    binding.valueTV.setTextColor(getColor(R.color.teal_700))
                     Toast.makeText(this, "Color Changed Started", Toast.LENGTH_SHORT)
                         .show()
                 }
                 20000 -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    valueTV.setTextColor(getColor(R.color.purple_200))
+                    binding.valueTV.setTextColor(getColor(R.color.purple_200))
+                    binding.valueTV.textSize = 25F
                 }
                 30000 -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    valueTV.setTextColor(getColor(R.color.teal_700))
+                    binding.valueTV.setTextColor(getColor(R.color.teal_700))
+                    binding.valueTV.textSize = 30F
                 }
                 40000 -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    valueTV.setTextColor(getColor(R.color.teal_700))
+                    binding.valueTV.setTextColor(getColor(R.color.teal_700))
+                    binding.valueTV.textSize = 35F
                 }
                 50000 -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    valueTV.setTextColor(getColor(R.color.teal_700))
+                    binding.valueTV.setTextColor(getColor(R.color.teal_700))
                 }
                 60000 -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    valueTV.setTextColor(getColor(R.color.teal_700))
+                    binding.valueTV.setTextColor(getColor(R.color.teal_700))
+                    binding.valueTV.textSize = 45F;
                 }
                 else -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    valueTV.setTextColor(getColor(R.color.black))
+                    binding.valueTV.setTextColor(getColor(R.color.black))
                 }
             }
 
-        }
-        button2.setOnClickListener {
-            Log.d(Tag,  "Thread Started")
         }
     }
 }
